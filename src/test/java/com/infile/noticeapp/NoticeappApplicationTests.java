cf35107efcfee6c1a534a84c0b3d3ee118372cc2
DESKTOP-H5L9UK1\Clatd <axeldcc@gmail.com>
2024-02-19 18:52:17 -0600

    Initial commit

---
+++ src/test/java/com/infile/noticeapp/NoticeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infile.noticeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
