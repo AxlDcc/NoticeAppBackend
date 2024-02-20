b857ef18b9f84fbd759d20a3b5e70daecc023169
DESKTOP-H5L9UK1\Clatd <axeldcc@gmail.com>
2024-02-19 19:30:24 -0600

    Add notice category to context

---
+++ src/main/java/com/infile/noticeapp/entity/NoticeCategory.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.infile.noticeapp.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Clatd
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "NOTICE_CATEGORY")
+public class NoticeCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
+    private Integer id;
+    
+    @Column(name="name", length=100)
+    private String name;
+}
